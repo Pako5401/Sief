9f049d21510492ccf7b22e6c98e0c93e2b285803
Pako5401 <daner_sk8@hotmail.com>
2015-09-22 10:27:31 -0500

    Se agrega codigo sql usuarios
    Mas tarde se creara un archivo .sql

--- Sief/src/com/mx/sief/test/LoginTest.java
+++ Sief/src/com/mx/sief/test/LoginTest.java
@@ -15,3 +15,30 @@ package com.mx.sief.test;
 public class LoginTest {
     
 }
+
+====================================================
+/* Primer tabla de prueba para implementar el modulo de "Login"
+	@Fecha: 22/09/2015 10:18 am
+	@Usuario Francisco Júarez
+ */
+/*
+CREATE DATABASE sief;
+ USE sief;
+
+	CREATE TABLE usuarios(
+	id_usuario INT(5) NOT NULL AUTO_INCREMENT,
+    usuario VARCHAR(30) NOT NULL,
+    contrasenia VARCHAR(30) NOT NULL,
+    nombre VARCHAR(30) NOT NULL,
+    apellidos VARCHAR(40) NOT NULL,
+    tipo CHAR(1) NOT NULL,
+    estatus FLOAT(1),
+    PRIMARY KEY(id_usuario)
+	);
+    
+    INSERT INTO usuarios VALUES(1,'fjuarez','123','Francisco Pedro','Juárez Medrano','a',1);	
+    INSERT INTO usuarios VALUES(0,'gperez','1234','Gustavo','Perez Savedra','o',1);
+    INSERT INTO usuarios VALUES(3,'elópez','1234','Edgar','López Jimenez','o',0);
+	
+	*/
+====================================================
