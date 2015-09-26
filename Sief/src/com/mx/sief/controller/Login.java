a09001cd56bd44e4f45f130cd27d3605e7c1dfb4
Pako5401 <daner_sk8@hotmail.com>
2015-09-26 00:45:57 -0500

    Se conecta la primera vista de login, y se cahan los eventos de ella en este controlador.

--- Sief/src/com/mx/sief/controller/Login.java
+++ Sief/src/com/mx/sief/controller/Login.java
@@ -13,6 +13,8 @@ package com.mx.sief.controller;
 import com.mx.sief.model.Conexion;
 import com.mx.sief.model.LoginBean;
 import com.mx.sief.model.LoginDao;
+import com.mx.sief.view.LoginForm;
+import javax.swing.JOptionPane;
 
 /*
  * Clase en cargada de controlar la logica de negocio al loguearse,
@@ -21,27 +23,50 @@ import com.mx.sief.model.LoginDao;
  * Escuchara los eventos creados en la interfaz.
  */
 public class Login {
-
+    Conexion con=null;
+    LoginDao ld =null;
     LoginBean lb = null;
     boolean estadoCon = true;
+    LoginForm loginForm;
 
+    
     public static void main(String[] args) {
-        Login log = new Login();
-        log.valUsuarios();
+        Login log=new Login();
+        log.iniciar();
     }
+    
+    /**
+	 * Permite instanciar todas las clases con las que trabaja
+	 * el sistema
+	 */
+	private void iniciar() {
+		/*Se instancian las clases*/
+		loginForm=new LoginForm();
+		loginForm.setVisible(true);
+	}
 
-    public void valUsuarios() {
-        Conexion con = new Conexion();
-        LoginDao ld = new LoginDao();
+    public void valUsuarios(String user,String pass) {
+        System.out.println("user = "+user);
+        System.out.println("pass = "+pass);
+        con = new Conexion();
+        ld = new LoginDao();
+//        lb=new LoginBean();
         estadoCon = con.valConexion();
-
         if (estadoCon == false) {
             System.out.println("Fallo la conexion a bd, Intenta mas tarde");
-        } else {
-            lb = ld.valUsuarios("fjuarez", "123");
+        } else if(estadoCon == true) {
+            lb = ld.valUsuarios(user, pass);
+            if(lb!=null){
             System.out.println("Nombre = " + lb.getNombre());
             System.out.println("Apellido = " + lb.getApellido());
             System.out.println("Tipo = " + lb.getTipo());
+            JOptionPane.showMessageDialog(null,"Bienvenido "+lb.getNombre()+" "+lb.getApellido());
+            } else {
+                JOptionPane.showMessageDialog(null,"Usuario incorrecto");
+            }
         }
     }
+
+
+
 }
